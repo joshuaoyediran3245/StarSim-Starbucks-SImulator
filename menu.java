f6bc53ca17f138d91fd80daf67c7075d0172b767
joshuaoyediran3245 <109435858+joshuaoyediran3245@users.noreply.github.com>
2024-03-10 20:57:03 -0400

    Creating the Menu
    Started to create the menu. Within the menu class I decided it is best to use a HashMap which is a data structure. A hashmap contains keys and values. I put comments for each section in the code that explains it.
    
    Co-Authored-By: LucasGaitan0507 <160949499+LucasGaitan0507@users.noreply.github.com>

---
+++ menu.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+//Even though we did not go over HashMap Data Structure
+// After doing futher research a HashMap would make more sense for displaying the item name and price 
+//The HashMap can store in a key and a value. 
+//In this case the key would be the item name ex. Caffe Americano. And the value would be the price $2.5
+
+
+
+
+public class Menu {
+//Create the map top store menu items with their prices
+
+private Map<String, Double> beverages; //One map for beverages
+private Map<String, Double> foods; // One map for foods
+
+
+//Constructor to initialize menu items 
+
+public void ListMenu() 
+{
+  beverages = new HashMap<>(); //Set the beverages to a HashMap
+  foods = new HashMap<>(); //Set the foods to a HashMap
+
+  //Populate Beverages
+
+  beverages.put("Caffe Americano", 2.50); //Key Food/Item name. Value = Price
+  beverages.put("Caffe Latte", 3.00);
+  beverages.put("Cappuccino", 3.00);
+  beverages.put("Espresso", 2.00);
+  beverages.put("Green Tea", 3.50); 
+
+
+  //Populate Foods
+
+  foods.put("Croissant", 2.00);
+  foods.put("Muffin", 2.50);
+  foods.put("Sandwich", 4.0);
+  foods.put("Salad", 5.00);
+  foods.put("Cake", 3.50); 
+
+}
+
+//Here is the method used to display the Menu
+public void displayMenu() 
+{
+System.out.println("==== Starbucks Menu ===="); //Using SOP to display the "Graphics" for the menu
+System.out.println("Beverages: ");
+
+//This for-each loop iterates over the keys of the 'beverages' map. This beverages map contains the names of the beverages set to keys and their prices set to values
+for (String beverage : beverages.keySet())
+{
+System.out.printf("%-20s $%.2f%n", beverage, beverages.get(beverage));
+}
+
+
+
+
+}
+
+}
